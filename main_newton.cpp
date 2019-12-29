a88f0d0890ee334e987c0214cf229d5a5ac7ffec
AliZainoul <Ali.Zainoul.az@gmail.com>
2019-12-29 21:25:59 +0100

    Stabilizing things

--- main_newton.cpp
+++ main_newton.cpp
@@ -8,11 +8,28 @@ using namespace std;
 
 typedef double (*pfn) (double);
 
-double p(double x){return (x*x)-1;};
-double pprime(double x){return (2*x);};
+double f(double x){return (1/x) - pow(2,x);};
+double fprime(double x){return ((-1/(2*x) ) -exp(x*log(2)));};
 
-double q(double x){return (x*x*x)-(5*x*x)+(3*x)+7;};
-double qprime(double x){return (3*x*x)-(10*x)+(3);};
+double g(double x){return (pow(2,(-x)) + exp(x) + 2 * cos(x) - 6);};
+double gprime(double x){return ( exp(-x*log(2)) + exp(x) - 2 * sin(x) );};
+
+double p(double x){return (x+1);};
+double pprime(double x){return 1;};
+
+double q(double x){return (x*x)-1;};
+double qprime(double x){return (2*x);};
+
+double r(double x){return (x*x*x)-(5*x*x)+(3*x)+7;};
+double rprime(double x){return (3*x*x)-(10*x)+(3);};
+
+double s(double x){return 1-(x*x);};
+double sprime(double x){return (-2*x);};
+
+double h(double x){return x;};
+double hprime(double x){return 1;};
+double jprime(double x){return 0;};
+
 
 
 
@@ -21,13 +38,42 @@ double qprime(double x){return (3*x*x)-(10*x)+(3);};
 int main ()
 {
 cout << "*** *** *** *** ***" << "\n";
-cout << "Test of polynom p :"<< "\n";
-cout << newton(0.5, p, pprime, 0.000001, 0.000001, 1000000) << "\n";
+cout << "Test of Function f :"<< "\n";
+cout << newton(1.5, f, fprime, 0.000001, 0.000001, 1000000) << "\n";
 cout << "*** *** *** *** ***" << "\n";
 
 cout << "*** *** *** *** ***" << "\n";
-cout << "Test of polynom q :"<< "\n";
-cout << newton(5, q, qprime, 0.000001, 0.000001, 1000000) << "\n";
+cout << "Test of Function g :"<< "\n";
+cout << newton(2, g, gprime, 0.000001, 0.000001, 1000000) << "\n";
 cout << "*** *** *** *** ***" << "\n";
 
+cout << "*** *** *** *** ***" << "\n";
+cout << "Test of Function p :"<< "\n";
+cout << newton(9999, p, pprime, 0.000001, 0.000001, 1000000) << "\n";
+cout << "*** *** *** *** ***" << "\n";
+
+cout << "*** *** *** *** ***" << "\n";
+cout << "Test of Function q :"<< "\n";
+cout << newton(0.1, q, qprime, 0.000001, 0.000001, 1000000) << "\n";
+cout << "*** *** *** *** ***" << "\n";
+
+cout << "*** *** *** *** ***" << "\n";
+cout << "Test of Function r :"<< "\n";
+cout << newton(5, r, rprime, 0.000001, 0.000001, 1000000) << "\n";
+cout << "*** *** *** *** ***" << "\n";
+
+cout << "*** *** *** *** ***" << "\n";
+cout << "Test of Function s :"<< "\n";
+cout << newton(-0.1, s, sprime, 0.000001, 0.000001, 1000000) << "\n";
+cout << "*** *** *** *** ***" << "\n";
+
+cout << "*** *** *** *** ***" << "\n";
+cout << "Test of Function h :"<< "\n";
+cout << newton(500, h, hprime, 0.000001, 0.000001, 1000000) << "\n";
+cout << "*** *** *** *** ***" << "\n";
+
+cout << "*** *** *** *** ***" << "\n";
+cout << "Test of Function j :"<< "\n";
+cout << newton(500, hprime, jprime, 0.000001, 0.000001, 1000000) << "\n";
+cout << "*** *** *** *** ***" << "\n";
 }
--- newton.cpp
+++ newton.cpp
@@ -5,15 +5,61 @@
 using namespace std;
 typedef double (*pfn) (double);
 
+bool root(pfn f, double xp)
+{
+  if (f(xp)==0.0) return true;
+  else return false;
+};
+
+bool divisionbyzero(pfn fd, double xp)
+{
+if (fd(xp)==0.0) return true;
+else return false;
+};
+
+bool haveWeFoundSolution(double xp, double xn)
+{
+if (xn==xp) return false;
+else return true;
+};
 double newton(double xp, pfn f, pfn fd, double delta, double epsilon, int n_max)
 {
-  double xn = xp - (1/fd(xp))*f(xp);
-  unsigned int k=0;
-  while (    ((abs(xn-xp)) > delta) && ((abs(f(xn)))  > epsilon)      )
+  //cout << divisionbyzero(fd,xp);
+  if (root(f,xp)) return xp;
+  if (!divisionbyzero(fd, xp))
   {
-    k += 1;
-    if (k<=n_max) return newton(xn, f, fd, delta, epsilon, n_max);
-    else error ("Too Many Iterations");
+    double xn = xp - (f(xp)/fd(xp));
+    while (    ((abs(xn-xp)) > delta) && ((abs(f(xn)))  > epsilon)     )
+    {
+      return newton(xn, f, fd, delta, epsilon, n_max-1);
+    }
+    if (haveWeFoundSolution(xp,xn)) return xn;
+    else {error("Method Diverge");}
   }
-  return xn;
+  else {error("Divisiong By Zero Occurs");}
 }
+
+/*
+double newton(double xp, pfn f, pfn fd, double delta, double epsilon, int n_max)
+{
+  bool haveWeFoundSolution = false;
+  double xn ;
+  for (unsigned int k=0; k<=n_max; k++)
+  {
+    double y=f(xp);
+    double yprime=fd(xp);
+    if ((abs(yprime))  < epsilon ) error ("Denominator too small.");
+    double xn = xp - (y/yprime);
+    if ((abs(xn-xp)) < delta)
+    {
+      haveWeFoundSolution = true;
+      break;
+    }
+    xp = xn;
+  }
+  if (haveWeFoundSolution) {cout << "The Method has converged ?" << haveWeFoundSolution << "\n";}
+  else error ("Method did not converge or too Many Iterations");
+  return xn;
+}*/
+
+//var = (k<=n_max) ? 30 : 40;
